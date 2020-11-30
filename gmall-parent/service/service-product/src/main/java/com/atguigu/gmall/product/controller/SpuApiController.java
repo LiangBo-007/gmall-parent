0cb4bd5cf9cd379c2b05876265a719a7d9a2b15a
liangbo <821728600@qq.com>
2020-11-30 11:46:31 +0800

    1.搭建框架修改配置 2.分类查询数据展示 3.基本查询

--- gmall-parent/service/service-product/src/main/java/com/atguigu/gmall/product/controller/SpuApiController.java
+++ gmall-parent/service/service-product/src/main/java/com/atguigu/gmall/product/controller/SpuApiController.java
@@ -1,9 +1,16 @@
 package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.model.product.SpuInfo;
+import com.atguigu.gmall.product.service.SpuInfoService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @description:
@@ -14,14 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class SpuApiController {
+    @Autowired
+    SpuInfoService spuInfoService;
+
     /**
      * @description:获取商品分页详情
      * @return:
      * @time: 2020/11/30 11:17
      * @author: LIANG BO
      */
-    @RequestMapping("{page}/{limit}/{category3Id}")
-    private Result getpage() {
-        return null;
+    @RequestMapping("{pageNo}/{size}")
+    private Result getpage(
+            @PathVariable("PageNo") Long PageNo,
+            @PathVariable("size") Long size,
+            @PathVariable("category3Id") Long category3Id
+    ) {
+        Page<SpuInfo> Page = new Page<>();
+        Page.setSize(size);
+        Page.setCurrent(PageNo);
+        List<SpuInfo> spuInfoList = spuInfoService.getpage(Page, category3Id);
+        return Result.ok(spuInfoList);
     }
 }
